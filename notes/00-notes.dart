73988df9564942a70740af07e48aa8208a7478d2
Alsayed Aldkhakhni <alsayedaldkhakhni@gmail.com>
2025-03-23 21:43:23 +0200

    Change location.

---
+++ notes/00-notes.dart
@@ -0,0 +1,62 @@
+/*
+ * File name: 00-notes.dart
+ * 
+ * Subject  : Keep notes of new concepts.
+ * 
+ * Date     : March 21, 2025
+ * 
+ * Author   : Alsayed A. Khaleel
+ */
+
+/* 1st
+ * What is Dart?
+ 
+ * It a high-level programming language developed by Google at 2011
+ * It existed to be a successor that replaces the weaknesses of JS.
+ * It was web-oriented language, but now it is mobile-apps oriented.
+ */
+
+/* 2nd
+ * What is Flutter?
+ 
+ * Is a framework [some predefined packages and widgets] that
+    uses Dart as its core.
+ * It is also developed by Google.
+ * It is used to develop the front-end of the app.
+ * It is used to develop the UI and how the app looks and behaves.
+ * It is used to develop apps [mobile, web, desktop, server-side].
+ * It uses Dart to maintain the app's logic.
+ */
+
+/* 3rd
+ * Why Flutter uses Dart?
+ 
+ * modern
+ * cross-platform
+ * continually improves its features
+ * general-purpose language
+ */
+
+/* 4th
+ * Dart code may be compiled into 2 ways
+   1st: using dart2js compiler.
+   2nd: using Dart VM.
+ */
+
+/* 5th
+ * Dart compilers.
+ 
+ * JIT: Just  In Time, used while writing or debugging an app.
+ * AOT: Ahead Of Time, used while building the app for release,
+        which in this case it is shipped with a tiny VM that provides
+        as a runtime library and a garbage collector.
+ */
+
+/* 6th
+ * Dart is as Python, Everything is an Object [defined or you do]
+ */
+
+/*
+ * Methods in Dart as those in C++, member methods defined it the caller class.
+   e.g. intVar == y you can though it as intVar.==(y)
+ */
